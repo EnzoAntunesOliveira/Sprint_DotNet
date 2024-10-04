f9404d94a2ec08d6d4ee52cb4cf5a76e3413b8f8
Enzo Antunes Oliveira <enzo2004antunes@gmail.com>
2024-10-04 13:53:49 -0300

    Tentando arrumar 404

--- fraude-odontologica/Program.cs
+++ fraude-odontologica/Program.cs
@@ -7,43 +7,57 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("FiapOracleConnection")));
+
 
 builder.Services.AddScoped<IRepository<Paciente>, PacienteRepository>();
 builder.Services.AddScoped<IRepository<Dentista>, DentistaRepository>();
 builder.Services.AddScoped<IRepository<Consulta>, ConsultaRepository>();
 builder.Services.AddScoped<IRepository<Tratamento>, TratamentoRepository>();
 
-
 builder.Services.AddScoped<DentistaService>();
 builder.Services.AddScoped<ConsultaService>();
 builder.Services.AddScoped<TratamentoService>();
+builder.Services.AddScoped<PacienteService>();
+
 
 
 builder.Services.AddControllers();
 
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Fraude Odontológica API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fraude Odontológica API v1");
-        c.RoutePrefix = string.Empty; 
+         c.RoutePrefix = string.Empty; // Se tirar o coment o Swagger vira primeira página
     });
-}
 
-app.UseHttpsRedirection();
+
+
+//app.UseHttpsRedirection(); 
+
 app.UseAuthorization();
+
+
 app.MapControllers();
+
 app.Run();
